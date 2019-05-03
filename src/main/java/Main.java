55c36ec2a5345b4c412b384bc6af9e4d0fdff649
YanQianyu <284989344@qq.com>
2019-05-03 17:29:34 +0800

    A little modify.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,33 +18,34 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException{
         listClasses(new File(dir));
 
-        final int threadNum = 5; // 开5个线程, 每个线程处理一个project, 每个线程的处理结果写入一个文件，最后进行merge处理
-        ArrayList<MultiVisitor> multiVisitors = new ArrayList<>();
-
+        final int threadNum = 2; // 开5个线程, 每个线程处理一个project, 每个线程的处理结果写入一个文件，最后进行merge处理
+        ArrayList<ArrayList<String>> splitProj = new ArrayList<>();
+        for(int k = 0; k < threadNum; k++)
+            splitProj.add(new ArrayList<String>());
         int i = 0;
-        for(i = 0; i < listProj.size(); i+= threadNum) {
-            for (int k = 0; k < threadNum; k++){
-                multiVisitors.add(new MultiVisitor(listProj.get(i + k), "t" + String.valueOf(k + 1)));
-            }
-            for (int k = 0; k < threadNum; k++){
-                multiVisitors.get(k).start();
-            }
+        for (i = 0; i< listProj.size() / threadNum * threadNum; i+=threadNum){
+            for(int k = 0; k < threadNum; k++)
+                splitProj.get(k).add(listProj.get(i + k));
         }
-
         for(; i < listProj.size(); i++){
-            MultiVisitor multiVisitor1 = new MultiVisitor(listProj.get(i),"t1");
-            multiVisitor1.start();
+            splitProj.get(0).add(listProj.get(i));
         }
+        ArrayList<MultiVisitor> multiVisitors = new ArrayList<>();
+        for(int k = 0; k < threadNum; k++){
+            multiVisitors.add(new MultiVisitor(splitProj.get(k), "t" + String.valueOf(k + 1)));
+        }
+        for(int k = 0; k < threadNum; k++)
+            multiVisitors.get(k).start();
 
     }
 }
 
 class MultiVisitor implements Runnable{
     private Thread t;
-    private String projPath;
+    private ArrayList<String> projPath;
     private String index;
 
-    MultiVisitor(String projPath, String index){
+    MultiVisitor(ArrayList<String> projPath, String index){
         this.index = index;
         this.projPath = projPath;
         System.out.println("Dealing " + projPath);
@@ -52,23 +53,25 @@ class MultiVisitor implements Runnable{
 
     @Override
     public void run() {
-        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file)  -> {
-            System.out.println(file.getPath());
-            try {
-                CompilationUnit cu = JdtAstUtil.getCompilationUnit(file.getPath());
-                MyVisitor myVisitor = new MyVisitor(index);
-                cu.accept(myVisitor);
-                System.out.println();
-            } catch (Exception e) {
-                System.err.println(e.toString());
-            }
-        }).explore(new File(projPath));
+        for (int i = 0; i < projPath.size(); i++){
+            new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file)  -> {
+                System.out.println(file.getPath());
+                try {
+                    CompilationUnit cu = JdtAstUtil.getCompilationUnit(file.getPath());
+                    MyVisitor myVisitor = new MyVisitor(index);
+                    cu.accept(myVisitor);
+                    System.out.println();
+                } catch (Exception e) {
+                    System.err.println(e.toString());
+                }
+            }).explore(new File(projPath.get(i)));
+        }
     }
 
     public void start(){
         System.out.println("Starting ");
         if(t == null){
-            t = new Thread(this, projPath);
+            t = new Thread(this);
             t.start();
         }
     }
